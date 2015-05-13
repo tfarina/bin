c0b53b398f3249142467e516d4415e9dafc2ad07
Thiago Farina <tfarina@chromium.org>
2015-05-13 17:35:43 -0300

    Fix cppcheck install script.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- install_cppcheck.sh
+++ install_cppcheck.sh
@@ -7,9 +7,9 @@ VERSION=1.69
 CPPCHECK_VERSION=cppcheck-$VERSION
 cd
 wget http://downloads.sf.net/project/cppcheck/cppcheck/$VERSION/$CPPCHECK_VERSION.tar.gz
+tar -zxvf $CPPCHECK_VERSION.tar.gz
 cd $CPPCHECK_VERSION
-g++ -o cppcheck -std=c++0x -include lib/cxx11emu.h -Iexternals/tinyxml -Ilib cli/*.cpp lib/*.cpp externals/tinyxml/*.cpp
-sudo install -D --verbose --backup=none --mode=755 -T cppcheck /usr/local/bin/cppcheck
+sudo make install
 cd ..
 rm -rf $CPPCHECK_VERSION
 rm $CPPCHECK_VERSION.tar.gz
