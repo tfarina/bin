c9ddcc9cf418f52eb43778b05a7d1324c72ac06a
Thiago Farina <tfarina@chromium.org>
2015-11-03 17:46:56 -0200

    Update Bazel to 0.0.1
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- install_bazel.sh
+++ install_bazel.sh
@@ -3,7 +3,7 @@
 echo "Installing Bazel..."
 echo
 
-VERSION=0.1.0
+VERSION=0.1.1
 wget https://github.com/bazelbuild/bazel/releases/download/${VERSION}/bazel-${VERSION}-installer-linux-x86_64.sh
 wget https://github.com/bazelbuild/bazel/releases/download/${VERSION}/bazel-${VERSION}-installer-linux-x86_64.sh.sha256
 sha256sum -c bazel-${VERSION}-installer-linux-x86_64.sh.sha256
