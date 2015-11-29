e13fceac47ddd9fb27a35ea9ad6b16653e310067
Thiago Farina <tfarina@chromium.org>
2015-11-29 17:32:13 -0200

    update install_bazel.sh script to version 0.1.2
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- install_bazel.sh
+++ install_bazel.sh
@@ -3,9 +3,8 @@
 echo "Installing Bazel..."
 echo
 
-VERSION=0.1.1
-wget https://github.com/bazelbuild/bazel/releases/download/${VERSION}/bazel-${VERSION}-installer-linux-x86_64.sh
-wget https://github.com/bazelbuild/bazel/releases/download/${VERSION}/bazel-${VERSION}-installer-linux-x86_64.sh.sha256
-sha256sum -c bazel-${VERSION}-installer-linux-x86_64.sh.sha256
-chmod +x bazel-${VERSION}-installer-linux-x86_64.sh
-./bazel-${VERSION}-installer-linux-x86_64.sh --user
+wget https://storage.googleapis.com/bazel/0.1.2/rc2/bazel-0.1.2rc2-installer-linux-x86_64.sh
+wget https://storage.googleapis.com/bazel/0.1.2/rc2/bazel-0.1.2rc2-installer-linux-x86_64.sh.sha256
+sha256sum -c bazel-0.1.2rc2-installer-linux-x86_64.sh.sha256
+chmod +x bazel-0.1.2rc2-installer-linux-x86_64.sh
+./bazel-0.1.2rc2-installer-linux-x86_64.sh --user
