92a7308d2d4cf82d4df4aa68afd36549d4c85624
Thiago Farina <tfransosi@gmail.com>
2021-02-26 14:07:29 -0500

    also change the group of the font files
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- install_hack_font.sh
+++ install_hack_font.sh
@@ -5,8 +5,9 @@ sudo mkdir -p /usr/share/fonts/truetype/hack
 cd /tmp
 wget -O hack-font.zip https://github.com/chrissimpkins/Hack/releases/download/v2.020/Hack-v2_020-ttf.zip
 unzip -o hack-font.zip
+
 for file in Hack-Bold.ttf Hack-BoldItalic.ttf Hack-Italic.ttf Hack-Regular.ttf; do
-  sudo chown root "${file}"
+  sudo chown root:root "${file}"
   sudo mv "${file}" /usr/share/fonts/truetype/hack
 done
 
